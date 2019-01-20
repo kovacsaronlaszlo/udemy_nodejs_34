e13fb25b2abf69a146486c6c9881af7ce95de4b8
Kovács Áron László <k.aron.laszlo@gmail.com>
2019-01-20 17:37:18 +0100

    delete useless files (pug, html and hbs)

--- 5._worikng_with_dinamy_content_&_adding_templateing_engines/routes/shop.js
+++ 5._worikng_with_dinamy_content_&_adding_templateing_engines/routes/shop.js
@@ -9,18 +9,15 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     const products = adminData.products;
     // res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
-    res.render(
-        'shop',
-        {
-            prods: products,
-            pageTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true,
-            // layout: false
-        }
-    );
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true,
+        // layout: false
+    });
 });
 
 module.exports = router;
